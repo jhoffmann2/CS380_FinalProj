79c30aea999832c47ae985f8f37c26fbf7f94ba0
dylan.pautler <dylan.pautler@digipen.edu>
2021-03-27 21:00:21 -0700

    adds door & pressureplate functionality

---
+++ CooperativePlanningDemo/Assets/PressurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public bool depressed;
+    [SerializeField] private Collider agent1Collider;
+    [SerializeField] private Collider agent2Collider;
+    void Start()
+    {
+        depressed = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Collider otherCollider = other.gameObject.GetComponent<Collider>();
+        
+        if (otherCollider == agent1Collider || otherCollider == agent2Collider)
+            depressed = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.GetComponent<Collider>() == agent1Collider || other.gameObject.GetComponent<Collider>() == agent2Collider)
+            depressed = false;
+    }
+}
