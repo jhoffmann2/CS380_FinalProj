38c0631438debe7605eb194cec0e84cdc7c5c129
UnableImp <39175158+UnableImp@users.noreply.github.com>
2021-03-27 17:07:28 -0700

    Added dynamic NavMesh and IOperation to navagate to points

---
+++ CooperativePlanningDemo/Assets/Planning/Operations/NavMeshToOperation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Planning.Operations
+{
+    public class NavMeshToOperation : IOperation
+    {
+        private GoapAgent goapAgentagent;
+        private NavMeshAgent navMeshAgent;
+        private Vector3 goal;
+        
+        public NavMeshToOperation(GoapAgent agent, Vector3 goal)
+        {
+            this.goapAgentagent = agent;
+            this.navMeshAgent = agent.transform.GetComponent<NavMeshAgent>();
+            this.goal = goal;
+        }
+
+        public void Init()
+        {
+            navMeshAgent.SetDestination(goal);
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Exit()
+        {
+        }
+
+        public bool IsComplete()
+        {
+            return !navMeshAgent.pathPending && !navMeshAgent.hasPath;
+        }
+    }
+}
