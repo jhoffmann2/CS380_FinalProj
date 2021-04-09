530a09306ced1268babe952934945a29ceeeb837
UnableImp <39175158+UnableImp@users.noreply.github.com>
2021-04-08 23:43:08 -0700

    q - to start, e - to reset

---
+++ CooperativePlanningDemo/Assets/DebugTools.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugTools : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            Time.timeScale = 1.0f;
+        if (Input.GetKeyDown(KeyCode.E))
+            SceneManager.LoadScene("SampleScene");
+    }
+}
